56fe8f6090d60e8dd11b998197d71ec00bdfeffb
Ayham <ali1ali2ali123@hotmail.com>
2021-09-20 19:21:17 +0300

    -update

--- CourseApp/filters.py
+++ CourseApp/filters.py
@@ -1,16 +1,28 @@
 from django_filters import rest_framework as filters
 from .models import *
 
+# class CourseFilter(filters.FilterSet):
+#     class Meta:
+#         model = Course
+#         fields = {'course_type', 'price'}
+
+CHOICES = (('online', 1), ('offline', 0),)
+
+
+def strtoint(val):
+    val = val.lower()
+    if val in ('online', '1'):
+        return 1
+    elif val in ('offline', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
 
 class CourseFilter(filters.FilterSet):
+    course_type = filters.TypedChoiceFilter(choices=CHOICES, coerce=strtoint)
+    price = filters.RangeFilter()
+
     class Meta:
         model = Course
         fields = {'course_type', 'price'}
-
-
-# class CourseFilter(django_filters.FilterSet):
-#     course_type = django_filters.NumberFilter()
-#     price = django_filters.NumberFilter()
-#
-#     class Meta:
-#         model = Course
